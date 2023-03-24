339b0eb91b7e0a0365f8eaf51524fe3afc0c9f61
Sean <seanhan18@hotmail.com>
2023-03-24 16:13:21 -0500

    added social media icons

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,37 @@
 import React from 'react'; 
+import reactDom from 'react-dom';
+import { SocialIcon } from 'react-social-icons';
 
 const Home = () => {
     return (
         <div id='FlexContainer' >
         <h1 id='MainTitle' >Sean Hannan's Portfolio Site</h1>
+            <SocialIcon 
+                className='SocialIcon'
+                url="https://github.com/SeanHan18" 
+                network="github"
+                target='_blank'
+                rel="noopener"
+                aria-label='Github'
+                bgColor="#059C49"
+                fgColor="#ffffff"
+            >
+            </SocialIcon>
+            <SocialIcon
+                className='SocialIcon'
+                url="https://www.linkedin.com/in/hannanseanr/" 
+                network="linkedin"
+                target='_blank'
+                rel="noopener"
+                aria-label='LinkedIn'
+                bgColor="#059C49"
+                fgColor="#ffffff"
+            >
+            </SocialIcon>
         </div>
     )
     }
+
+
 
 export default Home
