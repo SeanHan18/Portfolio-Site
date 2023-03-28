29a5629816c7e918cfd006edeab861ad09a3b7e1
Sean <seanhan18@hotmail.com>
2023-03-28 14:55:00 -0500

    Added changes to navbar links on mouse hover

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom'
 
+
 const Navbar = () => {
+    const [homeHovered, setHomeHovered] = useState(false);
+    const toggleHomeHover = () => setHomeHovered(!homeHovered);
+    const [aboutHovered, setAboutHovered] = useState(false);
+    const toggleAboutHover = () => setAboutHovered(!aboutHovered);
+    const [projectsHovered, setProjectsHovered] = useState(false);
+    const toggleProjectsHover = () => setProjectsHovered(!projectsHovered);
+    const [contactHovered, setContactHovered] = useState(false);
+    const toggleContactHover = () => setContactHovered(!contactHovered)
     return (
         <header>
             <nav className='nav-bar'>
                 <ul id='navlist' > 
-                <Link to='/home' className='navlink'>Home</Link>
-                <Link to='/about' className='navlink'>About</Link>
-                <Link to='/projects' className='navlink'>Projects</Link>
-                <Link to='/contact' className='navlink'>Contact</Link>
+                <Link to='/home'  className={homeHovered ? 'selectednav' : 'navlink'} onMouseEnter={toggleHomeHover} onMouseLeave={toggleHomeHover}>Home</Link>
+                <Link to='/about' className={aboutHovered ? 'selectednav' : 'navlink'} onMouseEnter={toggleAboutHover} onMouseLeave={toggleAboutHover}>About</Link>
+                <Link to='/projects' className={projectsHovered ? 'selectednav' : 'navlink'} onMouseEnter={toggleProjectsHover} onMouseLeave={toggleProjectsHover}>Projects</Link>
+                <Link to='/contact' className={contactHovered ? 'selectednav' : 'navlink'} onMouseEnter={toggleContactHover} onMouseLeave={toggleContactHover}>Contact</Link>
                 </ul>
             </nav>
         </header>
