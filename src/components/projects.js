3488e3e649ab2a5fb01f1f33321d3117dc6ae799
Sean <seanhan18@hotmail.com>
2023-04-03 16:25:21 -0500

    Added new projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,22 +1,30 @@
 import React from 'react'; 
+import { SocialIcon } from 'react-social-icons';
+
 
 const Projects = () => {
     return (
         <div id='FlexContainer' >
         <div id= 'Project'>
-        <h3 id='ProjectTitle' >Project 1</h3>
-        <div id='ProjectText'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna id volutpat lacus laoreet non curabitur gravida. Aliquam purus sit amet luctus venenatis lectus. Pulvinar mattis nunc sed blandit libero volutpat. A diam maecenas sed enim ut sem. Vitae congue eu consequat ac felis donec. Lectus proin nibh nisl condimentum id venenatis a condimentum. Leo vel fringilla est ullamcorper eget nulla facilisi. Velit egestas dui id ornare arcu. In dictum non consectetur a. Odio pellentesque diam volutpat commodo sed egestas.</div>
+        <h3 id='ProjectTitle' >Stranger's Things</h3>
+        <div id='ProjectText'>A Craigslist-lite application that allows users to facilitate the buying and selling of items. Includes the ability for users to register and create accounts, post item offers, and send messages to one another. Developed with HTML5, CSS, React.js, and Express.</div>
+        <span id='ProjectText'>Project Github Link:&nbsp;<a
+        href='https://github.com/SeanHan18/StrangersThings' target="_blank">Stranger's Things</a></span>
         </div>
         <div id= 'Project'>
-        <h3 id='ProjectTitle' >Project 2</h3>
-        <div id='ProjectText'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna id volutpat lacus laoreet non curabitur gravida. Aliquam purus sit amet luctus venenatis lectus. Pulvinar mattis nunc sed blandit libero volutpat. A diam maecenas sed enim ut sem. Vitae congue eu consequat ac felis donec. Lectus proin nibh nisl condimentum id venenatis a condimentum. Leo vel fringilla est ullamcorper eget nulla facilisi. Velit egestas dui id ornare arcu. In dictum non consectetur a. Odio pellentesque diam volutpat commodo sed egestas.</div>
+        <h3 id='ProjectTitle' >StaleFish</h3>
+        <div id='ProjectText'>A mock digital storefront for purchasing skating goods. Users can create accounts and purchase items through a cart system. Also allows for admin accounts that can create and edit store products. Developed with HTML5, CSS, React.js, Express, and Axios.</div>
+        <span id='ProjectText'>Project Github Link:&nbsp;<a
+        href='https://github.com/SeanHan18/StaleFish-2' target="_blank">StaleFish</a></span>
         </div>
         <div id= 'Project'>
-        <h3 id='ProjectTitle' >Project 3</h3>
-        <div id='ProjectText'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna id volutpat lacus laoreet non curabitur gravida. Aliquam purus sit amet luctus venenatis lectus. Pulvinar mattis nunc sed blandit libero volutpat. A diam maecenas sed enim ut sem. Vitae congue eu consequat ac felis donec. Lectus proin nibh nisl condimentum id venenatis a condimentum. Leo vel fringilla est ullamcorper eget nulla facilisi. Velit egestas dui id ornare arcu. In dictum non consectetur a. Odio pellentesque diam volutpat commodo sed egestas.</div>
+        <h3 id='ProjectTitle' >Arcade</h3>
+        <div id='ProjectText'>Includes playable Snake and Tic-Tac-Toe games coded in Javascript.</div>
+        <span id='ProjectText'>Project Github Link:&nbsp;<a
+        href='https://github.com/SeanHan18/arcade' target="_blank">Arcade</a></span>
         </div>
         <div id= 'Project'>
-        <h3 id='ProjectTitle' >Project 4</h3>
+        <h3 id='ProjectTitle' >To Be Added (Placeholder)</h3>
         <div id='ProjectText'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna id volutpat lacus laoreet non curabitur gravida. Aliquam purus sit amet luctus venenatis lectus. Pulvinar mattis nunc sed blandit libero volutpat. A diam maecenas sed enim ut sem. Vitae congue eu consequat ac felis donec. Lectus proin nibh nisl condimentum id venenatis a condimentum. Leo vel fringilla est ullamcorper eget nulla facilisi. Velit egestas dui id ornare arcu. In dictum non consectetur a. Odio pellentesque diam volutpat commodo sed egestas.</div>
         </div>
         </div>
