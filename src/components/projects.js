6f1a8229e07dc6362130103cd33410d14acecace
Sean <seanhan18@hotmail.com>
2023-06-05 16:25:50 -0500

    Added Stalefish screenshot

--- src/components/projects.js
+++ src/components/projects.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { SocialIcon } from 'react-social-icons';
 import ArcadeScreen from '../images/Arcade Screenshot 1.png'
 import StrangersScreen from '../images/Strangers Things Screenshot 2.png'
-
+import StaleFishScreen from '../images/Stalefish Logo.png'
 
 const Projects = () => {
     return (
@@ -15,6 +15,7 @@ const Projects = () => {
         href='https://github.com/SeanHan18/StrangersThings' target="_blank">Stranger's Things</a></span>
         </div>
         <div id= 'Project2'>
+        <img id= 'ProjectImage' style={{ width:200, height:200}} src={StaleFishScreen} alt="Stalefish ScreenShot" />    
         <h3 id='ProjectTitle' >StaleFish</h3>
         <div id='ProjectText'>A mock digital storefront for purchasing skating goods. Users can create accounts and purchase items through a cart system. Also allows for admin accounts that can create and edit store products. Developed with HTML5, CSS, React.js, Express, and Axios.</div>
         <span id='ProjectText'>Project Github Link:&nbsp;<a
@@ -27,10 +28,10 @@ const Projects = () => {
         <span id='ProjectText'>Project Github Link:&nbsp;<a
         href='https://github.com/SeanHan18/arcade' target="_blank">Arcade</a></span>
         </div>
-        <div id= 'Project4'>
+        {/* <div id= 'Project4'>
         <h3 id='ProjectTitle' >To Be Added (Placeholder)</h3>
         <div id='ProjectText'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna id volutpat lacus laoreet non curabitur gravida. Aliquam purus sit amet luctus venenatis lectus. Pulvinar mattis nunc sed blandit libero volutpat. A diam maecenas sed enim ut sem. Vitae congue eu consequat ac felis donec. Lectus proin nibh nisl condimentum id venenatis a condimentum. Leo vel fringilla est ullamcorper eget nulla facilisi. Velit egestas dui id ornare arcu. In dictum non consectetur a. Odio pellentesque diam volutpat commodo sed egestas.</div>
-        </div>
+        </div> */}
         </div>
     )
     }
